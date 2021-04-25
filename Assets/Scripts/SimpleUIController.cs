00445227decc448c4571cddce71e569eb1f613d8
azaliyaraisovna <azaliyaraisovna@gmail.com>
2021-04-25 12:22:40 +0300

    add Intro

--- Assets/Scripts/SimpleUIController.cs
+++ Assets/Scripts/SimpleUIController.cs
@@ -7,15 +7,23 @@ using UnityEngine.UI;
 public class SimpleUIController : MonoBehaviour
 {
     [SerializeField] private Canvas _psy;
+    [SerializeField] private Canvas _intro;
     [SerializeField] private Canvas _menu;
     [SerializeField] private Button _start;
     [SerializeField] private Button[] _levels;
     [SerializeField] private Button _backButton;
 
+
+    private const float INTROTIMER = 8;
+
     private bool _inMenu;
+    private bool _inIntro;
+
+    private float _timerIntro;
 
     private void Awake()
     {
+        _timerIntro = 0.0f;
         _menu.gameObject.SetActive(true);
         _psy.gameObject.SetActive(false);
 
@@ -26,6 +34,7 @@ public class SimpleUIController : MonoBehaviour
         }
 
         _backButton.onClick.AddListener(GoToMenu);
+        _start.onClick.AddListener(GoToIntro);
     }
 
     private void GoToLevel(int i)
@@ -38,6 +47,15 @@ public class SimpleUIController : MonoBehaviour
         SceneLoader.Instance.LoadLevel(i);
     }
 
+    private void GoToIntro()
+    {
+        _inMenu = false;
+        _inIntro = true;
+
+        _menu.gameObject.SetActive(_inMenu);
+        _psy.gameObject.SetActive(_inMenu);
+        _intro.gameObject.SetActive(!_inMenu);
+    }
     private void GoToMenu()
     {
         _inMenu = true;
@@ -52,5 +70,25 @@ public class SimpleUIController : MonoBehaviour
     {
         if (!_inMenu && Input.GetKeyDown(KeyCode.Escape))
             GoToMenu();
+
+        if (_inIntro && _timerIntro <= INTROTIMER)
+            _timerIntro += Time.deltaTime;
+        else if (_inIntro)
+        {
+            _inMenu = true;
+            _inIntro = false;
+
+            _menu.gameObject.SetActive(_inMenu);
+            _intro.gameObject.SetActive(!_inMenu);
+
+            _start.gameObject.SetActive(_inIntro);
+
+            for (int i = 0; i < _levels.Length; i++)
+            {
+                var index = i + 1;
+                _levels[i].gameObject.SetActive(_inMenu);
+            }
+
+        }
     }
 }
