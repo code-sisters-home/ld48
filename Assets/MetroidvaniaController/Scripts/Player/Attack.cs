7814faa5f3bf8820b72ac059e6e7076c993ada7f
azaliyaraisovna <azaliyaraisovna@gmail.com>
2021-04-26 13:33:24 +0300

    add donut and arrow

--- Assets/MetroidvaniaController/Scripts/Player/Attack.cs
+++ Assets/MetroidvaniaController/Scripts/Player/Attack.cs
@@ -38,7 +38,7 @@ public class Attack : MonoBehaviour
 
 		if (Input.GetKeyDown(KeyCode.Z))
 		{
-			GameObject throwableWeapon = Instantiate(throwableObject, transform.position + new Vector3(transform.localScale.x * 0.5f,-0.2f), Quaternion.identity) as GameObject; 
+			GameObject throwableWeapon = Instantiate(throwableObject, transform.position + new Vector3(transform.localScale.x * 0.5f, 0.0f), Quaternion.identity) as GameObject; 
 			Vector2 direction = new Vector2(transform.localScale.x, 0);
 			throwableWeapon.GetComponent<ThrowableWeapon>().direction = direction; 
 			throwableWeapon.name = "ThrowableWeapon";
